c71c745fe4abed85660be2d44efa72554f7cc343
suzuki-developer <suzuki_dev@outlook.com>
2023-08-30 10:46:51 +0900

    12章（練習問題以外完了）

---
+++ スッキリわかるJava入門/Chapter_12_10/Main.java
@@ -0,0 +1,14 @@
+package Chapter_12_10;
+
+public class Main {
+    public static void main(String[] args) {
+        Monster[] monsters = new Monster[3];
+        monsters[0] = new Slime();
+        monsters[1] = new Goblin();
+        monsters[2] = new DeathBat();
+
+        for (Monster m : monsters) {
+            m.run();
+        }
+    }
+}
